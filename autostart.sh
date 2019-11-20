289e549d188a3edea93bb04e1523469ca8237224
Tintamar <martin.hermant@gmail.com>
2019-11-20 10:44:40 +0100

    Add Sessions

--- autostart.sh
+++ autostart.sh
@@ -4,10 +4,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BASEDIR=$DIR/..
 
 cd $BASEDIR/vermuth/server
-NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME &
+sudo NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME &
 
 cd $BASEDIR/Epau
-./run.sh &
+sudo ./run.sh &
 
 
 
