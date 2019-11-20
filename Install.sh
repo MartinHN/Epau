0c009b769ec62cc5b99d712b76853ce11ec0a3f8
Tintamar <martin.hermant@gmail.com>
2019-11-20 13:28:54 +0100

    WUIP

--- Install.sh
+++ Install.sh
@@ -2,4 +2,6 @@ sudo apt-get install python3-pip
 sudo apt-get install python3-pygame
 sudo apt install libsdl1.2-dev
 
+sudo systemctl enable pigpiod
+
 alsamixer!!
--- autostartPython.sh
+++ autostartPython.sh
@@ -5,9 +5,6 @@ HOMEDIR=/home/pi
 BASEDIR=$HOMEDIR
 
 cd $BASEDIR/Epau
-set -e
-sudo pigpiod
-set +e
 python3 main.py >> /tmp/python.log 2>&1 
 
 
