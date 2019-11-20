88f75e17d4c7801b27bc130a74f4d2a262d4ba65
Tintamar <martin.hermant@gmail.com>
2019-11-20 13:19:02 +0100

    WIP

--- autostartPython.sh
+++ autostartPython.sh
@@ -4,11 +4,8 @@ HOMEDIR=/home/pi
 
 BASEDIR=$HOMEDIR
 
-cd $BASEDIR/vermuth/server
-NODE_ENV=production /usr/bin/node -r tsconfig-paths/register -r ts-node/register ./src/server.ts --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME >> /tmp/vermuth.log 2>&1 &
-
 cd $BASEDIR/Epau
-sudo ./run.sh >> /tmp/python.log 2>&1 &
+sudo ./run.sh >> /tmp/python.log 2>&1 
 
 
 
