223d681714ed28660e2f5ea1d6ed32898ee44747
Tintamar <martin.hermant@gmail.com>
2019-11-20 11:58:43 +0100

    WIP

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 exec &> "${HOME}/epaulog.txt"
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BASEDIR=$DIR/..
+
+BASEDIR=$HOME
 
 cd $BASEDIR/vermuth/server
 sudo NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME &
