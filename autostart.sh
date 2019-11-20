699287bdf384587a7560d0a88e42bff24d068eaa
Tintamar <martin.hermant@gmail.com>
2019-11-20 12:31:03 +0100

    WIP

--- autostart.sh
+++ autostart.sh
@@ -8,7 +8,7 @@ cd $BASEDIR/vermuth/server
 sudo NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME >> /tmp/vermuth.log 2>&1 &
 
 cd $BASEDIR/Epau
-sudo ./run.sh >> /tmp/python.log 2>&1 &
+sudo ./run.sh >> /tmp/python.log 2>&1 
 
 
 
