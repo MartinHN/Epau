7e9c549b1a7b57511ffd1ca6b93abf58f43347b8
Tintamar <martin.hermant@gmail.com>
2019-11-20 12:51:17 +0100

    WIP

--- autostart.sh
+++ autostart.sh
@@ -5,10 +5,11 @@ HOMEDIR=/home/pi
 BASEDIR=$HOMEDIR
 
 cd $BASEDIR/vermuth/server
-sudo NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME >> /tmp/vermuth.log 2>&1 &
+sudo NODE_ENV=production /usr/bin/node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME >> /tmp/vermuth.log 2>&1 &
 
 cd $BASEDIR/Epau
-sudo ./run.sh >> /tmp/python.log 2>&1 
+sudo ./run.sh >> /tmp/python.log 2>&1 &
+
 
 
 
