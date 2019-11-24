830f4b326edcb74e84f411911e08ddc6991ed37a
Tintamar <martin.hermant@gmail.com>
2019-11-24 10:40:52 +0100

    Fix services

--- autostartVermuth.sh
+++ autostartVermuth.sh
@@ -5,4 +5,4 @@ HOMEDIR=/home/pi
 BASEDIR=$HOMEDIR
 
 cd $BASEDIR/vermuth/server
-NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME --public="${BASEDIR}/vermuth/server/dist/server/public" &> /tmp/vermuth.log 
+NODE_ENV=production node dist/server/src/server.js --path="${BASEDIR}/Epau/vermuth/" --session=$HOSTNAME --public="${BASEDIR}/vermuth/server/dist/server/public" 
